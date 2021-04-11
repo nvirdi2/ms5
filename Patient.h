d4fb525068fda92884ceb6732feb393acc08f55e
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-04-10 21:14:47 -0400

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -1,52 +1,32 @@
-// Name: Navdeep Virdi
-// Seneca email: nvirdi2@myseneca.ca
-// Student ID: 166485193
-// Date: April 10th, 2021
-
-//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-
 #ifndef SDDS_PATIENT_H
 #define SDDS_PATIENT_H
-
-#include <iostream>
-
 #include "IOAble.h"
 #include "Ticket.h"
-
-namespace sdds 
-{
-    class Patient: public IOAble 
-    {
-            Ticket object;
-
-            char* m_name = nullptr;
-            int o_num;
-            bool flag;
-
+#include <iostream>
+namespace sdds {
+    class Patient: public IOAble{
+            char* m_pname = nullptr;
+            int ohip_no;
+            Ticket obj;
+            bool io_flag;
         public:
-            Patient(int NUM = 0, bool check = false);
+            Patient(int number = 0, bool flag = false);
             ~Patient();
             Patient(const Patient&) = delete;
-
             void operator=(const Patient&) = delete;
             void getSpecs(std::istream&);
+            virtual char type() const = 0;
+            bool fileIO() const;
             void fileIO(bool);
-            void setArrivalTime();
-
             bool operator ==(char) const;
             bool operator ==(const Patient&) const;
-            bool fileIO() const;
-
-            std::istream& read(std::istream& is);
-            std::istream& csvRead(std::istream& is);
-            std::ostream& write(std::ostream& os) const;
-            std::ostream& csvWrite(std::ostream& os) const;
-
-            virtual char type() const = 0;
-            
-            int number() const;
-
+            void setArrivalTime();
             operator Time() const;
+            int number() const;
+            std::istream& read(std::istream& istr);
+            std::ostream& write(std::ostream& ostr) const;
+            std::istream& csvRead(std::istream& istr);
+            std::ostream& csvWrite(std::ostream& ostr) const;
     };
 }
-#endif //!SDDS_PATIENT_H
+#endif
