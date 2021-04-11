4ab3cff21c1268274c3ab887275c64d0e82f19d3
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-04-10 21:12:36 -0400

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,64 +1,39 @@
-// Name: Navdeep Virdi
-// Seneca email: nvirdi2@myseneca.ca
-// Student ID: 166485193
-// Date: April 10th, 2021
-
-//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstring>
-#include <iostream>
-
 #include "Menu.h"
 #include "utils.h"
-
+#include <cstring>
 using namespace std;
-namespace sdds 
-{
-    Menu::Menu(const char* MenuContent, int NoOfSelections) 
+namespace sdds {
+    void Menu::setText(const char* text)
+    {
+        int len = strlen(text);
+        m_text = new char[len + 1];
+        strcpy(m_text, text);
+        m_text[len] = '\0';
+    }
+    Menu::Menu(const char* MenuContent, int NoOfSelections)
     {
         setText(MenuContent);
-            m_noOfSel = NoOfSelections;
+        m_noOfSel = NoOfSelections;
     }
-
-    Menu::~Menu() 
+    Menu::~Menu()
     {
         delete[] m_text;
     }
-
-    Menu::Menu(const Menu& other) 
+    Menu::Menu(const Menu &other)
     {
         setText(other.m_text);
-            m_noOfSel = other.m_noOfSel;
-    }
-
-
-
-    void Menu::setText(const char* text) 
-    {
-        int txt = strlen(text);
-        m_text = new char[txt + 1];
-
-        strcpy(m_text, text);
-
-        m_text[txt] = '\0';
-    }
-
-    void Menu::display()const 
+        m_noOfSel = other.m_noOfSel;
+    }  
+    void Menu::display()const
     {
         cout << m_text << endl;
-            cout << "0- Exit" << endl;
-    }
-
-
-
-    int& Menu::operator>>(int& Selection) 
+        cout << "0- Exit" << endl;
+    } 
+    int& Menu::operator>>(int& Selection)
     {
         display();
-
-        int correct = getInt(0, m_noOfSel, "> ", "Invalid option ");
-        Selection = correct;
-
-            return Selection;
+        int val = getInt(0, m_noOfSel, "> ", "Invalid option ");
+        Selection = val;
+        return Selection;
     }
 }
