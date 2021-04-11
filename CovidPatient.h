d9dd17401d2ad25a3aacdc32897e76695ad0c347
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-04-10 21:58:39 -0400

    Update CovidPatient.h

--- CovidPatient.h
+++ CovidPatient.h
@@ -1,31 +1,14 @@
-// Name: Navdeep Virdi
-// Seneca email: nvirdi2@myseneca.ca
-// Student ID: 166485193
-// Date: April 10th, 2021
-
-//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-
 #ifndef SDDS_COVIDPATIENT_H_
 #define SDDS_COVIDPATIENT_H_
-
 #include "Patient.h"
-
-namespace sdds 
-{
-    class CovidPatient : public Patient 
-    {
-    public:
-        CovidPatient();
-
-        char type() const;
-
-        std::istream& read(std::istream& is);
-        std::istream& csvRead(std::istream& is);
-        
-        std::ostream& write(std::ostream& os) const;
+namespace sdds {
+    class CovidPatient : public Patient{
+        public:
+            CovidPatient();
+            char type() const;
+            std::istream& read(std::istream& istr);
+            std::ostream& write(std::ostream& ostr) const;
+            std::istream& csvRead(std::istream& istr);
     };
 }
-#endif //!SDDS_COVIDPATIENT_H_
-
-
-
+#endif // !SDDS_COVIDPATIENT_H_
