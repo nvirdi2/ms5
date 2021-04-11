f6b23ec65ae541329463cd12392840a2f9e6aa7b
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-04-10 21:19:40 -0400

    Update Ticket.h

--- Ticket.h
+++ Ticket.h
@@ -1,20 +1,36 @@
+// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: April 10th, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 #ifndef SDDS_TICKET_H_
 #define SDDS_TICKET_H_
-#include "Time.h"
+
 #include "IOAble.h"
-namespace sdds {
-   class Ticket:public IOAble{
-      Time m_time;
-      int m_number;
+#include "Time.h"
+
+namespace sdds 
+{
+   class Ticket:public IOAble 
+   {
+      Time time;
+      int num;
    public:
-      Ticket(int number = 0);
-      operator Time()const;
-      int number()const;
+      Ticket(int NUM = 0);
+
       void resetTime();
-      std::ostream& csvWrite(std::ostream& ostr)const;
-      std::istream& csvRead(std::istream& istr);
-      std::ostream& write(std::ostream& ostr )const;
-      std::istream& read(std::istream& istr);
+
+      std::istream& read(std::istream& is);
+      std::istream& csvRead(std::istream& is);
+
+      std::ostream& csvWrite(std::ostream& os)const;
+      std::ostream& write(std::ostream& os )const;
+
+      operator Time()const;
+
+      int number()const;
    };
 }
-#endif // !SDDS_TICKET_H_
+#endif //!SDDS_TICKET_H_
