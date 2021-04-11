cb48971cc31ffa0c93ac56a0ec0ca44d1ae1184d
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-04-10 22:32:22 -0400

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -60,23 +60,25 @@ namespace sdds
         char i = '\0';
 
         istr >> Hours;
-        if (!(Hours > 0)) {
+        if(Hours < 0)
+        {
             istr.setstate(ios::failbit);
             return istr;
         }
 
         istr >> i;
-        if (!(i == ':')) {
+        if (!(i == ':')) 
+        {
             istr.setstate(ios::failbit);
             return istr;
         }
 
         istr >> Minutes;
-        if (Minutes < 0) {
+        if(Minutes < 0)
+        {
             istr.setstate(ios::failbit);
             return istr;
         }
-
         m_minutes = Hours * 60 + Minutes;
         return istr;
     }
