0b4c0c88c41e1a12036e4e9920d13fa2d577eeaa
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-04-10 21:14:28 -0400

    Update Patient.cpp

--- Patient.cpp
+++ Patient.cpp
@@ -1,137 +1,92 @@
-// Name: Navdeep Virdi
-// Seneca email: nvirdi2@myseneca.ca
-// Student ID: 166485193
-// Date: April 10th, 2021
-
-//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-
 #define _CRT_SECURE_NO_WARNINGS
-
+#include "Patient.h"
+#include "utils.h"
 #include <cstring>
 #include <string>
-
-#include "utils.h"
-#include "Patient.h"
-
-namespace sdds 
-{
-    Patient::Patient(int NUM, bool check):object(NUM) 
+namespace sdds {
+    Patient::Patient(int number, bool flag):obj(number)
     {
-        flag = check;
+        io_flag = flag;
     }
-    Patient::~Patient() 
+    Patient::~Patient()
     {
-        delete[] m_name;
+        delete[] m_pname;
     }
-
-
-
-    void Patient::fileIO(bool check) 
+    bool Patient::fileIO() const
     {
-        flag = check;
+        return io_flag;
     }
-    void Patient::setArrivalTime() 
+    void Patient::fileIO(bool flag)
     {
-        object.resetTime();
+        io_flag = flag;
     }
-
-
-    bool Patient::operator ==(char x) const 
+    bool Patient::operator ==(char ch) const
     {
-            return x == type();
+        return ch == type();
     }
-    bool Patient::operator ==(const Patient& any) const 
+    bool Patient::operator ==(const Patient& other) const
     {
-            return type() == any.type();
+        return type() == other.type();
     }
-    bool Patient::fileIO() const 
+    void Patient::setArrivalTime()
     {
-            return flag;
+        obj.resetTime();
     }
-
-
-
-    std::istream& Patient::read(std::istream& is) 
+    Patient::operator Time() const
     {
-        int lenght;
-
+        return Time(obj);
+    }
+    int Patient::number() const
+    {
+        return obj.number();
+    }
+    std::ostream& Patient::csvWrite(std::ostream& ostr) const
+    {
+        ostr << type() << ',';
+        ostr <<m_pname << ',';
+        ostr << ohip_no << ',';
+        return obj.csvWrite(ostr);
+    }
+    std::istream& Patient::csvRead(std::istream& istr)
+    {
+        std::string name = "";
+        istr.ignore();
+        getline(istr, name, ',');
+        int len = name.length();
+        delete[] m_pname;
+        m_pname = new char[len + 1];
+        strcpy(m_pname, name.c_str());
+        m_pname[len] = '\0';
+        
+        istr >> ohip_no;
+        istr.ignore();
+        return obj.csvRead(istr);
+    }
+    std::ostream& Patient::write(std::ostream& ostr) const
+    {
+        ostr << obj << '\n';
+        for(int i = 0; i<25 && m_pname[i]; i++)
+        {
+            ostr << m_pname[i];
+        }
+        ostr << ", OHIP: ";
+        ostr << ohip_no;
+        return ostr;
+    }
+    std::istream& Patient::read(std::istream& istr)
+    {
         std::cout << "Name: ";
+        
         std::string name;
-        std::getline(is, name);
-
-        lenght = name.length();
-
-        delete[] m_name;
-        m_name = new char[lenght + 1];
-        strcpy(m_name, name.c_str());
-
-        m_name[lenght] = '\0';
+        std::getline(istr, name);
+        int len = name.length();
+        delete[] m_pname;
+        m_pname = new char[len + 1];
+        strcpy(m_pname, name.c_str());
+        m_pname[len] = '\0';
 
         std::cout << "OHIP: ";
-        o_num = getInt(100000000, 999999999, nullptr, "Invalid OHIP Number, ");
-        
-            return is;
-    }
-    std::istream& Patient::csvRead(std::istream& is) 
-    {
-        int lenght;
-
-        std::string name;
-        std::getline(is, name, ',');
-
-        lenght = name.length();
-
-        delete[] m_name;
-
-        m_name = new char[lenght + 1];
-        strcpy(m_name, name.c_str());
-
-        m_name[lenght] = '\0';
-        
-        is >> o_num;
-        is.ignore();
-
-            return object.csvRead(is);
-    }
-
-
-
-    std::ostream& Patient::write(std::ostream& os) const
-    {
-        int i;
-
-        os << object << '\n';
-
-        for(i = 0; i < 25 && m_name[i]; i++)
-        {
-            os << m_name[i];
-        }
-
-        os << ", OHIP: ";
-        os << o_num;
-
-            return os;
-    }
-    std::ostream& Patient::csvWrite(std::ostream& os) const 
-    {
-        os << type() << ',';
-        os <<m_name << ',';
-        os << o_num << ',';
-
-            return object.csvWrite(os);
-    }
-
-
-
-    int Patient::number() const 
-    {
-            return object.number();
-    }
-
-
-
-    Patient::operator Time() const 
-    {
-            return Time(object);
+        ohip_no = getInt(100000000, 999999999, nullptr, "Invalid OHIP Number, ");
+        return istr;
     }
 }
